010dbcb29534c992cc0d5097c9dcbc2aa295f2b6
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-03-31 18:48:31 -0500

    fix: more const-correctness fies

--- src/generic_serial.rs
+++ src/generic_serial.rs
@@ -7,7 +7,7 @@ map_jump_table! {
     0xa54 => pub fn vexDeviceGenericSerialBaudrate(device: V5_DeviceT, baudrate: i32),
     0xa58 => pub fn vexDeviceGenericSerialWriteChar(device: V5_DeviceT, c: u8) -> i32,
     0xa5c => pub fn vexDeviceGenericSerialWriteFree(device: V5_DeviceT) -> i32,
-    0xa60 => pub fn vexDeviceGenericSerialTransmit(device: V5_DeviceT, buffer: *mut u8, length: i32) -> i32,
+    0xa60 => pub fn vexDeviceGenericSerialTransmit(device: V5_DeviceT, buffer: *const u8, length: i32) -> i32,
     0xa64 => pub fn vexDeviceGenericSerialReadChar(device: V5_DeviceT) -> i32,
     0xa68 => pub fn vexDeviceGenericSerialPeekChar(device: V5_DeviceT) -> i32,
     0xa6c => pub fn vexDeviceGenericSerialReceiveAvail(device: V5_DeviceT) -> i32,
