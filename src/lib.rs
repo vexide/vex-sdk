e8728318f19c628836707bcc5a49cc47eeb1a527
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-06 10:32:23 -0700

    feat: auto-generate safety section in jumptable function docs

--- src/lib.rs
+++ src/lib.rs
@@ -73,9 +73,10 @@ macro_rules! map_jump_table {
     ) => {
         $(
             $(#[$meta])*
+            #[doc = "# Safety\nCalls to jumptable functions are unsafe because jumptable functions are owned by VEXos and we cannot guarantee their safety."]
             #[no_mangle] $vis unsafe extern "C" fn $name($($arg: $arg_ty),*) $(-> $ret)? {
                 unsafe {
-                    (*((crate::JUMP_TABLE_START + $offset) as *const extern "C" fn($($arg_ty,)*) $(-> $ret)?))($($arg,)*)
+                    (*(($crate::JUMP_TABLE_START + $offset) as *const extern "C" fn($($arg_ty,)*) $(-> $ret)?))($($arg,)*)
                 }
             }
         )+
