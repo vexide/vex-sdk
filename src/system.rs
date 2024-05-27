a983b5a8d024b5c9c8febdf9102e1643f575e0a5
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-05-27 18:21:30 -0500

    feat: implement all the things for vcodesig

--- src/system.rs
+++ src/system.rs
@@ -12,6 +12,7 @@ use crate::map_jump_table;
 /// for example a virtual machine, may override the default settings to cause
 /// the V5 system code to enable custom functionality yet TBD.
 #[repr(C, packed)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct vcodesig {
     /// Magic, must be 'VXV5' 0x35565856 le
     pub magic: u32,
@@ -28,15 +29,30 @@ pub struct vcodesig {
     pub options: u32,
 }
 
+impl Default for vcodesig {
+    fn default() -> Self {
+        vcodesig {
+            magic: V5_SIG_MAGIC,
+            r#type: Default::default(),
+            owner: Default::default(),
+            pad: Default::default(),
+            options: Default::default(),
+        }
+    }
+}
+
 pub const V5_SIG_MAGIC: u32 = 0x35585658;
 pub const V5_SIG_TYPE_USER: u32 = 0;
 pub const V5_SIG_OWNER_SYS: u32 = 0;
 pub const V5_SIG_OWNER_VEX: u32 = 1;
 pub const V5_SIG_OWNER_PARTNER: u32 = 2;
 pub const V5_SIG_OPTIONS_NONE: u32 = 0;
-pub const V5_SIG_OPTIONS_INDG: u32 = (1 << 0);
-pub const V5_SIG_OPTIONS_EXIT: u32 = (1 << 1);
-pub const V5_SIG_OPTIONS_THDG: u32 = (1 << 2);
+/// Invert default graphics colors
+pub const V5_SIG_OPTIONS_INDG: u32 = 1 << 0;
+/// Kill threads when main exits
+pub const V5_SIG_OPTIONS_EXIT: u32 = 1 << 1;
+/// Invert graphics based on theme
+pub const V5_SIG_OPTIONS_THDG: u32 = 1 << 2;
 
 #[repr(C)]
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
