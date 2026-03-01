3eefa0179e4d8def72c2b4561c0a764bc8982328
Lewis McClelland <lewis@lewismcclelland.me>
2026-03-01 00:48:36 -0500

    feat: improve task docs & add safety specifiers

--- packages/vex-sdk/src/task.rs
+++ packages/vex-sdk/src/task.rs
@@ -3,13 +3,30 @@
 use core::ffi::{c_char, c_int, c_void};
 
 unsafe extern "system" {
-    pub fn vexTaskAdd(
-        callback: unsafe extern "system" fn() -> c_int,
+    /// Creates a cooperative VEXos task.
+    ///
+    /// The task can later be polled by calling [`vexTasksRun`] outside of a task.
+    pub unsafe fn vexTaskAdd(
+        callback: extern "system" fn() -> c_int,
         interval: c_int,
         label: *const c_char,
     );
-    pub fn vexTaskGetCallbackAndId(index: u32, callback_id: *mut c_int) -> *mut c_void;
-    pub fn vexTaskSleep(time: u32);
-    pub fn vexBackgroundProcessing();
-    pub fn vexTasksRun();
+    pub unsafe fn vexTaskGetCallbackAndId(index: u32, callback_id: *mut c_int) -> *mut c_void;
+    /// Yields the current task for the specified amount of time.
+    ///
+    /// This function will either switch execution to another task or to the last caller of
+    /// [`vexTasksRun`].
+    ///
+    /// # Safety
+    ///
+    /// Must be called from inside a task spawned by [`vexTaskAdd`] or an equivalent.
+    pub unsafe fn vexTaskSleep(time: u32);
+    /// No-op. In certain SDKs from VEX this function is an alias of [`vexTasksRun`].
+    ///
+    /// This function logs "Sdk Mismatch" to the system event log.
+    pub safe fn vexBackgroundProcessing();
+    /// Runs any pending VEXos tasks.
+    ///
+    /// These could be user-spawned tasks via [`vexTaskAdd`], or system tasks like
+    pub safe fn vexTasksRun();
 }
